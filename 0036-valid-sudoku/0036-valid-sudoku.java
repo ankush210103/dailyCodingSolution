2e1fc7feb9ca21a69fab0c0d8423edaa6b3cf8d3
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2024-07-10 01:09:34 +0530

    Time: 1 ms (100%), Space: 44.3 MB (53.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,15 +1,45 @@
 class Solution {
-   public boolean isValidSudoku(char[][] board) {
-    Set seen = new HashSet();
-    for (int i=0; i<9; ++i) {
-        for (int j=0; j<9; ++j) {
-            if (board[i][j] != '.') {
-                String b = "(" + board[i][j] + ")";
-                if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
-                    return false;
+
+    public static boolean isValid(char[][] board,int row,int col,char str){
+        for(int i=0;i<9;i++){
+            if(board[row][i] == str){
+                return false;
             }
         }
+        for(int i=0;i<9;i++){
+            if(board[i][col] == str){
+                return false;
+            }
+        }
+        int sRow = row/3*3;
+        int sCol  = col/3*3;
+        for(int i=sRow;i<sRow+3;i++){
+            for(int j=sCol;j<sCol+3;j++){
+                if(board[i][j] == str){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
-    return true;
-}
+   
+       
+   public boolean isValidSudoku(char[][] board) {
+    for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }else{
+
+                char str = board[i][j];
+                board[i][j] = '.';
+                if(isValid(board,i,j,str) == false){
+                    return false;
+                }
+                board[i][j] = str;
+                }
+            }
+        }
+        return true;
+    }
 }
