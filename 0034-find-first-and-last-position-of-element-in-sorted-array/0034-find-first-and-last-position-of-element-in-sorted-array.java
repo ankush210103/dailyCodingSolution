d4e855f186d2f5e239e138e41b1fa006076b8a7e
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-01-16 13:48:15 +0530

    Time: 0 ms (100%), Space: 45.8 MB (88.84%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,46 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[2];
-        arr[0] = -1;
-        arr[1] = -1;
-        if(nums.length == 0){
-            arr[0] = -1;
-            arr[1] = -1;
-        }
-        if(nums.length == 1){
+    public static int lowerBound(int[] arr,int target){
+        int low =0,high = arr.length-1;
+        // int mid = (low + high )/2;
+        int res = -1;
+        while(low<=high){
+             int mid = (low + high )/2;
 
-        }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                arr[0] = i;
-                arr[1] = i;
-                while(i<nums.length-1){
-                   
-                    if(nums[i+1] == target){
-                        arr[1] = i+1;
-                    }else{
-                       
-                        break;
-                    }
-                    i++;
-                }
+            if(arr[mid]  == target){
+              res = mid;
+                high = mid-1;
+            }else if(arr[mid]> target){
+                high = mid-1;
+            }else{
+              low = mid+1;
             }
-            
-
+            // mid = (low+high)/2;
         }
-        return arr;
-        
+        return res;
     }
+     public static int upperBound(int[] arr,int target){
+        int low =0,high = arr.length-1;
+        // int mid = (low + high )/2;
+        int res = -1;
+        while(low<=high){
+             int mid = (low + high )/2;
+
+            if(arr[mid]  == target){
+              res = mid;
+                low = mid+1;
+            }else if(arr[mid]> target){
+                high = mid-1;
+            }else{
+              low = mid+1;
+            }
+            // mid = (low+high)/2;
+        }
+        return res;
+    }
+    public int[] searchRange(int[] nums, int target) {
+            int[] arr = new int[2];
+            arr[0] = lowerBound(nums,target);
+            arr[1] = upperBound(nums,target);
+            return arr;
+    }   
 }
