98b79ad39942231ad596e4ca93b5fe2582cdfbe5
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-01-22 14:53:08 +0530

    Time: 0 ms (100%), Space: 42.7 MB (29.76%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1.0;
+        }
+
+        if(n<0){
+             if (n == Integer.MIN_VALUE) {
+                // Reduce the power by 1 to avoid overflow, then adjust the result
+                return 1 / (myPow(x, -(n + 1)) * x);
+            }
+            return 1 / myPow(x, -n);
+        }
+
+        double half = myPow(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }else{
+            return half * half* x;
+        }
+        
+    }
+}
