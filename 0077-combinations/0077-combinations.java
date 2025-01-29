0dee17b87b9bc55cf05ca96d31f8535cc41bd11b
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-01-30 01:06:36 +0530

    Time: 18 ms (80.14%), Space: 95 MB (13.54%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,24 +1,28 @@
 class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+       List<List<Integer>> res = new ArrayList<>();
+       solve(1,n,k,new ArrayList<>(),res); 
+       return res;
+    }
 
-    public static void returnCombine(int n,int k,int index,List<List<Integer>> res,List<Integer> response){
-        if(response.size() == k){
+
+    public static void solve(int i,int n,int k,List<Integer> response,List<List<Integer>> res){
+
+        if(k== 0){
             res.add(new ArrayList<>(response));
             return;
         }
-        if(index>n){
-            return;
-        }
-        returnCombine(n,k,index+1,res,response);
-        int element = index;
-        response.add(element);
-        returnCombine(n,k,index+1,res,response);
-        response.remove(response.size()-1);
-    }
 
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> response = new ArrayList<>();
-        returnCombine(n,k,1,res,response);
-        return res;
+        if(i>n) return;
+      
+
+     
+        response.add(i);
+        solve(i+1,n,k-1,response,res);
+        response.remove(response.size()-1);
+        solve(i+1,n,k,response,res);
+
+
+
     }
 }
