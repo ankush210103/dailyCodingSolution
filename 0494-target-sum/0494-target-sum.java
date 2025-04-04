39d8a29f3eee782cf67573a61e5feb22be4cc8d9
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-04-05 03:21:34 +0530

    Time: 4 ms (93.38%), Space: 43.4 MB (47.52%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,41 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, 0, 0);
-    }
+        int totalSum =0;
+        for(int i=0;i<nums.length;i++){
+            totalSum +=nums[i];
+        }
 
-    private int helper(int[] arr, int target, int idx, int sum) {
-        if (idx >= arr.length)
-            return sum == target ? 1 : 0;
-        
-        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
-        int add = helper(arr, target, idx + 1, sum + arr[idx]);
+        int t = (totalSum - target)/2;
+        if((totalSum-target)<0 || (totalSum-target)%2 != 0) return 0;
 
-        return subtract + add;
+        int[] prev = new int[t+1];
+        int[] curr = new int[t+1];
+        prev[0] = 1;
+        if(nums[0] == 0){
+            prev[0] = 2;
+        }
+       
+        curr[0] = 1;
+        if(nums[0] <=t && nums[0] != 0){
+            prev[nums[0]] = 1;
+        }
+
+        for(int i=1;i<nums.length;i++){
+            for(int j =0;j<=t;j++){
+                int notTake = prev[j];
+                int take = 0;
+                if(nums[i] <=j){
+                    take = prev[j-nums[i]];
+                }
+
+                curr[j] = take + notTake;
+            }
+            prev = curr.clone();
+        }
+
+        return prev[t];
+
+
+
     }
 }
