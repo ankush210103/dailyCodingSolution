685a12e47c8a2bab789dc90913baaea3eb2f6fee
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-04-05 17:19:13 +0530

    Time: 20 ms (75.13%), Space: 51.1 MB (30.48%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -19,10 +19,24 @@ class Solution {
         int[][] dp = new int[n+1][m+1];
 
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j] = -1;
+            dp[i][0] = 0;
+        }
+
+        for(int j=0;j<=m;j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j= 1;j<=m;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] =1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        return solve(n,m,text1,text2,dp);   
+        
+        return dp[n][m];   
     }
 }
