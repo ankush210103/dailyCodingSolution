f61a0691303e43b6b19ca1c94efdff905bf90bd1
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-04-10 12:48:44 +0530

    Time: 5 ms (86.89%), Space: 44 MB (42.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,40 +1,59 @@
 class Solution {
+
+    public static int solve(int index,int[] nums,int target){
+        if(index == 0){
+            if(target == 0 && nums[0] == 0) return 2;
+            if(target == 0 || target ==nums[0]) return 1;
+
+            return 0;
+        }
+
+        int notTake = solve(index-1,nums,target);
+        int take = 0;
+        if(nums[index] <= target){
+        take = solve(index-1,nums,target-nums[index]);
+        }
+
+        return take + notTake;
+    }
+
+
     public int findTargetSumWays(int[] nums, int target) {
-        int totalSum =0;
+        int totalSum = 0;
         for(int i=0;i<nums.length;i++){
-            totalSum +=nums[i];
+            totalSum+=nums[i];
         }
 
-        int t = (totalSum - target)/2;
-        if((totalSum-target)<0 || (totalSum-target)%2 != 0) return 0;
+      int sm = totalSum + target;
+        if(sm < 0 || sm % 2 != 0) return 0;
+        sm = sm / 2;
 
-        int[] prev = new int[t+1];
-        int[] curr = new int[t+1];
-        prev[0] = 1;
-        if(nums[0] == 0){
-            prev[0] = 2;
+        int n =nums.length;
+        
+        int[][] dp = new int[n][sm+1];
+        
+        if(nums[0] == 0) dp[0][0] =2;
+         if (nums[0] == 0) {
+        dp[0][0] = 2; // +0 and -0
+    } else {
+        dp[0][0] = 1; // Don't take the element
+        if (nums[0] <= sm) {
+            dp[0][nums[0]] = 1;
         }
-       
-        curr[0] = 1;
-        if(nums[0] <=t && nums[0] != 0){
-            prev[nums[0]] = 1;
-        }
-
-        for(int i=1;i<nums.length;i++){
-            for(int j =0;j<=t;j++){
-                int notTake = prev[j];
+    }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=sm;j++){
+                int notTake = dp[i-1][j];
                 int take = 0;
-                if(nums[i] <=j){
-                    take = prev[j-nums[i]];
+                if(nums[i] <= j){
+                    take = dp[i-1][j-nums[i]];
                 }
 
-                curr[j] = take + notTake;
+                dp[i][j] = take+notTake;
             }
-            prev = curr.clone();
         }
 
-        return prev[t];
-
+        return dp[n-1][sm];
 
 
     }
