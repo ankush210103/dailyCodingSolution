bca71d667c0e784aa790616548c78eadd537719a
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-04-13 14:39:57 +0530

    Time: 1 ms (76.94%), Space: 45.8 MB (57.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,53 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        if (prices.length < 2)
+
+    public static int solve(int index,int buy,int[] prices){
+        if(index == prices.length){
             return 0;
-        int ans = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                ans += prices[i] - prices[i - 1]; 
-            }
+        }
+        int profit =0;
+        if(buy == 1){
+            profit = Math.max(-prices[index] + solve(index+1,0,prices), solve(index+1,1,prices));
+        }else{
+            profit = Math.max(prices[index]+solve(index+1,1,prices),solve(index+1,0,prices));
         }
 
-        return ans;
+        return profit;
+    }
+
+
+
+    public int maxProfit(int[] prices) {
+        // if (prices.length < 2)
+        //     return 0;
+        // int ans = 0;
+        // for (int i = 1; i < prices.length; i++) {
+        //     if (prices[i] > prices[i - 1]) {
+        //         ans += prices[i] - prices[i - 1]; 
+        //     }
+        // }
+
+        // return ans;
+
+       // return solve(0,1,prices);
+       int n =prices.length;
+       int[][] dp = new int[n+1][2];
+       dp[n][0] = 0;
+       dp[n][1] = 0;
+
+       for(int i=n-1;i>=0;i--){
+        for(int buy= 0;buy<=1;buy++){
+            int profit =0;
+            if(buy == 1){
+                profit = Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            }else{
+                profit = Math.max(prices[i]+dp[i+1][1],dp[i+1][0]);
+            }
+
+            dp[i][buy] = profit;
+        }
+       }
+
+       return dp[0][1];
     }
 }
  
