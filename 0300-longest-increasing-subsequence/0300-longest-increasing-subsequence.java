926a77b491f6d5a1b43ccce2431553735a01e31c
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2025-04-17 00:21:06 +0530

    Time: 6 ms (88.49%), Space: 44.6 MB (38.64%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,38 +1,74 @@
 class Solution {
-    public static int solve(int index,int prev,int[] nums,int[][] dp){
-        if(index == nums.length) return 0;
+    
+    public static int lower(int low, int high, ArrayList<Integer> arr, int target) {
+    int ans = high + 1;
 
-        if(dp[index][prev+1] != -1) return dp[index][prev+1];
-        int notTake = solve(index+1,prev,nums,dp);
-        int take =0;
-        if(prev== -1 || nums[index]> nums[prev]){
-            take = 1 + solve(index+1,index,nums,dp);
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr.get(mid) >= target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
+    }
 
+    return ans;
+}
+
+
+    public static int solve(int index,int prev,int[] nums,int[][] dp){
+        if(index == nums.length){
+            return 0;
+        }
+        if(dp[index][prev+1] != -1){
+            return dp[index][prev+1];
+        }
+        int notTake = solve(index+1,prev,nums,dp);
+        int take = 0;
+        if(prev == -1 || nums[index] > nums[prev]){
+            take = 1+solve(index+1,index,nums,dp);
+        }
         return dp[index][prev+1] = Math.max(take,notTake);
     }
+
+
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n+1][n+1];
-
+       // int[][] dp = new int[n+1][n+1];
         // for(int i=0;i<n;i++){
         //     Arrays.fill(dp[i],-1);
         // }
         // return solve(0,-1,nums,dp);
 
 
-        for(int i= n-1;i>=0;i--){
-            for(int prev_idx = i-1;prev_idx>=-1;prev_idx--){
-                int notTake = dp[i+1][prev_idx+1];
+        // for(int i=n-1;i>=0;i--){
+        //     for(int prev_idx = i;prev_idx>=-1;prev_idx--){
 
-                int take =0;
-                if(prev_idx == -1 || nums[i] > nums[prev_idx]){
-                    take = 1 + dp[i+1][i+1];
-                }
-                dp[i][prev_idx+1] = Math.max(take,notTake);
+        //         int notTake = dp[i+1][prev_idx+1];
+        //         int take =0;
+        //         if(prev_idx == -1 || nums[i]> nums[prev_idx]){
+        //             take = 1 + dp[i+1][i+1];
+        //         }
+
+        //         return Math.max(take,notTake);
+        //     }
+        // }
+
+        // return dp[0][0];
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i] > arr.get(arr.size()-1)){
+                arr.add(nums[i]);
+            }else{
+                int index = lower(0,arr.size()-1,arr,nums[i]);
+               arr.set(index,nums[i]);
             }
         }
 
-        return dp[0][0];
+        return arr.size();
     }
 }
